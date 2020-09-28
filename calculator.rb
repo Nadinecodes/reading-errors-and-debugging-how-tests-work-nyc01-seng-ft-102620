a8b4888bc9207d800c9db7b67678dec6a3dda4f1
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-28 16:47:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number= 5.0
-second_number= 6.5
-sum= first_number + second
+second_number= 4.5
+sum= first_number + second_number
+difference= first_number - second_number
+product= first_number * second_number
+quotient = first_number/second_number
